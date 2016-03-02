0c07919b18af0638801323c0006a6ff3d343a9f4
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2016-03-01 21:26:46 -0800

    Added url & license to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ast-eq "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Verifies equality between two data structures with unique elements (eg: gensym)"
+  :url "https://github.com/Pancia/ast-eq"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
